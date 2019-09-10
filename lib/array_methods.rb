b5949d8b39c9f7f65738aaa1348222fa331f9a5e
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-10 20:16:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,10 +12,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  index == 0 
+  index = 0 
   for index in 0..array.length do
-    if(array[index]>array[index+1] && index != array.length)
-      return array[index];
+    if(array[index]>array[index+1])
+      return array[index]
+    end
+  end
 end
 
 def find_min_value(array)
